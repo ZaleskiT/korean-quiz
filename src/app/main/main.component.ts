031ad151a8bbe8707cceb78751c7c3f40f772b92
ZaleskiT <zaleskitimothy@yahoo.com>
2024-06-24 21:13:03 +0900

    part 7

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -10,6 +10,7 @@ import { Category } from '../data';
 })
 export class MainComponent {
   categories = [
+    { id: 8, name: 'Level 1 Class P.7 김치찌개 하나 주세요' },
     { id: 7, name: 'Level 1 Class P.6 9시부터 6시까지 일해요' },
     { id: 6, name: 'Level 1 Class P.5 오늘은 5월 5일이에요' },
     { id: 5, name: 'Level 1 Class P.4 랗흐만 씨가 식당에 가요' },
@@ -18,7 +19,7 @@ export class MainComponent {
     { id: 2, name: 'Level 1 Class P.1 안녕하세요?' },
     { id: 1, name: 'Level 0 Class' }
   ];
-  selectedCategoryMain: number = 7;
+  selectedCategoryMain: number = 8;
 
   constructor(private quizService: QuizService, private router: Router) { }
 
